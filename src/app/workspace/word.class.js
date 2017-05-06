1c000634e6ae07591f804f07d6fdf55522fb7d31
Alex Tabora <adtabora@gmail.com>
2017-05-05 18:07:01 -0600

    removing traspiled files from repository
    Former-commit-id: 080f3a1c19e4f7e2fee003fa2a0718c4bc7af9c0

--- src/app/workspace/word.class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tagged_word = (function () {
-    function tagged_word() {
-    }
-    return tagged_word;
-}());
-exports.tagged_word = tagged_word;
-//# sourceMappingURL=word.class.js.map
